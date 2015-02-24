39236435112ac555237d1d073b922f90fe626fe3
wired2015 <will.reid3@gmail.com>
2015-02-24 16:07:20 +0100

    Changed the CMakeLists.txt file so that it take all of the c files and header files in the directory. We do not have to manually define each file now.

--- generateCMakeLists.sh
+++ generateCMakeLists.sh
@@ -2,6 +2,7 @@ cd src
 echo "rock_library(single_leg_planner" > CMakeLists.txt
 echo "SOURCES" >> CMakeLists.txt
 ls *.cpp >> CMakeLists.txt
+echo "HEADERS" >> CMakeLists.txt
 ls *.h >> CMakeLists.txt 
 echo ")" >> CMakeLists.txt << EOF
 cd ..
